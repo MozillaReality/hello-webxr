68c277704dc5d5919bb5e0be13e612e2194d45a3
Fernando Serrano <fernandojsg@gmail.com>
2019-10-24 10:53:10 +0200

    Refactor telport

--- src/lib/Teleport.mjs
+++ src/lib/Teleport.mjs
@@ -6,43 +6,25 @@ var intersected = [];
 export default class Teleport {
   constructor(ctx, mesh) {
     this.ctx = ctx;
-    this.raycaster = new THREE.Raycaster();
-    this.colliderMesh = mesh;
-    this.targetPoint = new THREE.Vector3();
-    this.data = {
-      type: 'parabolic',
-      button: 'trackpad',
-      startEvents: [],
-      endEvents: [],
-      collisionEntities: '',
-      hitEntity: '',
-      cameraRig: '',
-      teleportOrigin: '',
-      hitCylinderColor: '#99ff99',
-      hitCylinderRadius: 0.25,
-      hitCylinderHeight: 0.3,
-      interval: 0,
-      maxLength: 10,
-      curveNumberPoints: 30,
-      curveLineWidth: 0.025,
-      curveHitColor: '#99ff99',
-      curveMissColor: '#ff0000',
-      curveShootingSpeed: 5,
-      defaultPlaneSize: 100,
-      landingNormal: new THREE.Vector3(0, 1, 0),
-      landingMaxAngle: 45,
-    }
 
     // Holder for all the
     this.teleportEntity = new THREE.Group();
 
     this.active = false;
-    this.line = this.createLine(this.data);
 
     this.teleportHitGeometry = new THREE.Mesh(
       new THREE.IcosahedronBufferGeometry(0.3, 1),
       new THREE.MeshBasicMaterial({color: 0xffff00, wireframe: true})
     );
+
+    this.ballColliding = new THREE.Mesh(
+      new THREE.IcosahedronBufferGeometry(0.02, 1),
+      new THREE.MeshBasicMaterial({color: 0xffffff})
+    );
+
+    this.ballColliding.visible = false;
+
+    this.teleportEntity.add(this.ballColliding);
 
     this.teleportHitGeometry.visible = false;
 
@@ -51,99 +33,36 @@ export default class Teleport {
     this.teleportEntity.add(this.teleportHitGeometry);
 
     this.ctx.scene.add(this.teleportEntity);
-
-    var geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
-    var material = new THREE.LineBasicMaterial({
-      color: 0x0000ff
-    });
-    var line = new THREE.Line( geometry, material );
-    line.name = 'line';
-    line.scale.z = 5;
-
-    this.line0 = line.clone();
-    this.line1 = line.clone();
-    this.line0.visible = this.line1.visible = true;
-
-    ctx.controllers[0].add( this.line0 );
-    //ctx.controllers[1].add( this.line1 );
-
   }
 
-  onSelectStart(evt) {
+  onSelectStart() {
     if (evt.target === this.ctx.controllers[0])
       this.active = true;
   }
 
-  getIntersections( controller ) {
-
-    tempMatrix.identity().extractRotation( controller.matrixWorld );
-
-    this.raycaster.ray.origin.setFromMatrixPosition( controller.matrixWorld );
-    this.raycaster.ray.direction.set( 0, 0, - 1 ).applyMatrix4( tempMatrix );
-
-    return this.raycaster.intersectObject( this.colliderMesh, true, null, true );
-
+  onHoverLeave() {
+    this.ballColliding.visible = false;
+    this.teleportHitGeometry.visible = false;
   }
 
-  intersectObjects(controller) {
-    if ( controller.userData.selected !== undefined ) return;
-
-    var line = controller.getObjectByName( 'line' );
-    var intersections = this.getIntersections( controller );
-
-    if ( intersections.length > 0 ) {
-
-      var intersection = intersections[ 0 ];
-
-      var object = intersection.object;
-      object.material.emissive.r = 1;
-      intersected.push( object );
-
-      line.scale.z = intersection.distance;
-      line.material.color.setRGB(1,1,0);
-
+  onHover(hitPoint, active) {
+    if (active) {
+      this.teleportHitGeometry.visible = true;
+      this.teleportHitGeometry.position.copy(hitPoint);
+      this.hit = true;
+      this.ballColliding.visible = false;
     } else {
-
-      line.scale.z = 5;
-      line.material.color.setRGB(1,0,0);
-
+      this.ballColliding.visible = true;
+      this.ballColliding.position.copy(hitPoint);
     }
   }
 
-  execute(ctx, delta, time) {
-    if (!this.active) { return; }
+  onSelectEnd(targetPoint) {
 
-    //for (var c=0;c<2;c++)
-    {
-      var controller = ctx.controllers[0];
-      var intersections = this.getIntersections(controller);
-
-      if (intersections.length > 0) {
-        this.targetPoint.copy(intersections[0].point);
-        this.teleportHitGeometry.visible = true;
-        this.teleportHitGeometry.position.copy(this.targetPoint);
-        this.hit = true;
-        return;
-      }
-    }
-  }
-
-  onSelectEnd() {
-
-    const teleportOriginWorldPosition = new THREE.Vector3();
-    const newRigLocalPosition = new THREE.Vector3();
-    const newHandPosition = [new THREE.Vector3(), new THREE.Vector3()]; // Left and right
-    const handPosition = new THREE.Vector3();
-
-    if (!this.active) { return; }
-
-    if (this.hit) {
-      this.ctx.cameraRig.position.copy(this.targetPoint);
-      this.teleportHitGeometry.visible = false;
-    }
+    this.ctx.cameraRig.position.copy(targetPoint);
+    this.teleportHitGeometry.visible = false;
 
     this.active = false;
-    this.hit = false;
   }
 
   createLine(data) {
