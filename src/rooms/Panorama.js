e801a52ec8def4128c6ec4bb2b3a99ee6af61adc
Diego F. Goberna <github@feiss.be>
2019-11-07 13:50:50 +0100

    added panel descriptions, tweaked sizes and included panels in panoramas

--- src/rooms/Panorama.js
+++ src/rooms/Panorama.js
@@ -1,8 +1,17 @@
 import * as THREE from 'three';
+import { Text, Object3D } from '../components/index.js';
 
-var pano = null, context;
+var pano = null, context, panel, panelText;
 
 const NUM_PANOS = 5;
+
+const DATA = [
+  'Tiger and Turtle - Magic Mountain\nArt installation in Agerpark, Germany.',
+  'Hiking trail at Lake Byllesby Regional Park near Cannon Falls, USA.',
+  'Dellwiger Bach natural reserve in Dortmund, Germany.',
+  'Zapporthorn summit in Lepontine Alps, Switzerland.',
+  'Ruin of romanesque Paulinzella abbey (1106) in Thuringia, Germany.',
+];
 
 var panoMaterials = [];
 
@@ -16,13 +25,39 @@ export function setup(ctx) {
     panoMaterials[i] = new THREE.MeshBasicMaterial( { map: assets[panoName], side: THREE.BackSide });
   }
   pano = new THREE.Mesh(geometry, panoMaterials[0]);
+
+  panel = assets['hall_model'].scene.getObjectByName('infopanel');
+  panel.material = new THREE.MeshBasicMaterial({color: 0x040404});
+  panel.position.set(0, 0.1, 0);
+  panel.parent.remove(panel);
+
+  panelText = ctx.world.createEntity();
+  panelText.addComponent(Text, {
+    color: '#ffffff',
+    fontSize: 0.02,
+    anchor: 'left',
+    textAlign: 'left',
+    baseline: 'center',
+    maxWidth: 0.34,
+    lineHeight: 1.3,
+    text: DATA[i],
+  });
+  let object3D = new THREE.Group();
+  panel.add(object3D);
+  object3D.position.set(-0.17, 0.003, 0.01);
+  panelText.addComponent(Object3D, {value: object3D});
 }
 
 export function enter(ctx) {
   ctx.renderer.setClearColor(0x000000);
-  console.log(ctx.room, panoMaterials[ctx.room - 5]);
-  pano.material = panoMaterials[ctx.room - 5];
+
+  const room = ctx.room - 5;
+  panelText.getMutableComponent(Text).text = DATA[room];
+  pano.material = panoMaterials[room];
+
   ctx.scene.add(pano);
+
+  ctx.controllers[1].add(panel);
 
   ctx.controllers[0].addEventListener('selectend', onSelectEnd);
   ctx.controllers[1].addEventListener('selectend', onSelectEnd);
@@ -32,6 +67,7 @@ export function enter(ctx) {
 
 export function exit(ctx) {
   ctx.scene.remove(pano);
+  ctx.controllers[1].remove(panel);
   ctx.controllers[0].removeEventListener('selectend', onSelectEnd);
   ctx.controllers[1].removeEventListener('selectend', onSelectEnd);
 }
--- src/stations/InfoPanels.js
+++ src/stations/InfoPanels.js
@@ -3,9 +3,83 @@ import { Text, Object3D } from '../components/index.js';
 
 var panels = [], panelTexts = [], cameraPosition;
 
-const NUM_PANELS = 13;
+const NUM_PANELS = 15;
 
 const SHOW_DISTANCE = 4;
+
+const DATA = [
+  {
+    title: '360 Panoramas',
+    description: 'Photographs wrapped around spheres provide an environment, but without stereo effect nor depth.',
+    offsety: 0.04
+  },
+  {
+    title: 'Xylophone Toy',
+    description: 'Example of object grabbing and simple interaction.',
+    offsety: 0.04
+  },
+  {
+    title: '360 Stereo Panorama',
+    description: 'By using one photo for each eye, panoramas can have some depth and stereo effect.',
+    offsety: 0.03
+  },
+  {
+    title: 'Photogrammetry Room',
+    description: 'Example of an object created out of photographs using an automated software.',
+    offsety: 0.04
+  },
+  {
+    title: 'Twitter Feed',
+    description: 'Bringing external realtime data to an XR environment.',
+    offsety: 0.02
+  },
+  {
+    title: 'Vertigo Room',
+    description: 'An example of how this new medium can play with your perception and senses.',
+    offsety: 0.04
+  },
+  {
+    title: 'Hello WebXR!',
+    description: 'A small compendium of interactions and little experiences introducing and celebrating the final specification of the WebXR API.\n \nMade by the Mozilla Mixed Reality team\nmixedreality.mozilla.org',
+    offsety: 0.06
+  },
+  {
+    title: 'The Pink Robe. After the Bath',
+    description: 'Joaquín Sorolla, 1916\n208 x 126.5 cm. Oil on canvas.',
+  },
+  {
+    title: 'The Garden of Earthly Delights',
+    description: 'Hieronymus Bosch, 1490 - 1510\n205.5 x 384.9 cm. Oil on oak panels.',
+  },
+  {
+    title: 'Self-Portrait',
+    description: 'Rembrandt van Rijn, 1659\n84.5 x 66 cm. Oil on canvas.',
+  },
+  {
+    title: 'The Dance Lesson',
+    description: 'Edgar Degas, 1879\n38 x 88 cm. Oil on canvas.',
+  },
+  {
+    title: 'Gray Weather, Grande Jatte',
+    description: 'Georges Seurat, 1888\n71 x 66 cm. Oil on canvas.',
+    offsety: 0.04
+  },
+  {
+    title: 'Sound Room',
+    description: 'Showcase of positional audio, very useful on XR experiences.\nTry to find where the sounds come from!.',
+    offsety: 0.04
+  },
+  {
+    title: 'Graffiti Wall',
+    description: 'Get close and spray the wall!.\nLet\'s collaborate on a full masterpiece.',
+    offsety: 0.04
+  },
+  {
+    title: 'Paintings',
+    description: 'Real-scale paintings that can be inspected very closely using the controller.',
+    offsety: 0.04
+  },
+];
 
 export function setup(ctx, hall) {
   const assets = ctx.assets;
@@ -17,6 +91,8 @@ export function setup(ctx, hall) {
 
     panels[i].geometry.computeBoundingBox();
     const panelWidth = panels[i].geometry.boundingBox.max.x - panels[i].geometry.boundingBox.min.x;
+    const panelHeight = panels[i].geometry.boundingBox.max.y - panels[i].geometry.boundingBox.min.y;
+    const offsety = DATA[i].offsety || 0;
     panels[i].material = new THREE.MeshBasicMaterial({color: 0x040404, transparent: true});
 
     panelTexts[i] = ctx.world.createEntity();
@@ -28,11 +104,11 @@ export function setup(ctx, hall) {
       baseline: 'top',
       maxWidth: panelWidth * 0.8,
       lineHeight: 1.3,
-      text: '360 PANORAMAS\n \nPhotos wrapped around spheres,\nno stereo effect.',
+      text: DATA[i].title + '\n \n' + DATA[i].description,
     });
     let object3D = new THREE.Group();
     panels[i].add(object3D);
-    object3D.position.set(-panelWidth / 2 * 0.8, panelWidth / 2 * 0.65, 0.01);
+    object3D.position.set(-panelWidth / 2 * 0.82, panelHeight / 2 * 0.65 + offsety, 0.01);
     panelTexts[i].addComponent(Object3D, {value: object3D});
   }
 }
