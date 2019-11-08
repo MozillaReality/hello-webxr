91e257239851970c4347e46f87e5d07f15f6fdce
Diego F. Goberna <github@feiss.be>
2019-11-08 11:53:36 +0100

    zoom aura

--- src/lib/shaders.js
+++ src/lib/shaders.js
@@ -31,7 +31,10 @@ void main( void ) {
   float t = time;
   vec2 uv = vec2(vUv.x - 0.5, (1.0 - vUv.y) - 0.5);
   vec4 col = texture2D(tex, zoomPos + uv * zoomAmount);
-  col.a = smoothstep(0.0, 0.1, 1.0 - length(uv) * 2.0);
+  float dist = length(uv) * 2.0;
+  col.a = smoothstep(0.0, 0.1, 1.0 - dist);
+  float aura = smoothstep(0.80, 1.0, dist);
+  col.rgb += aura * 0.3;
   gl_FragColor = col;
 }
 `,
