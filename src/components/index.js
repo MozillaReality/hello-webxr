6f62b88a0e37ce235b1bab72c34b80454cf75c55
Fernando Serrano <fernandojsg@gmail.com>
2019-11-08 20:33:20 +0100

    Componets for AreaChecker

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
+import * as THREE from 'three';
+import {TagComponent} from '../vendor/ecsy.module.js';
+
 export class Object3D {
   constructor() {
     this.value = null;
@@ -29,3 +32,56 @@ export class Text {
     this.text = '';
   }
 }
+
+export class BoundingBox {
+  constructor() {
+    this.min = new THREE.Vector3();
+    this.max = new THREE.Vector3();
+    // this.box3?
+  }
+
+  reset() {
+    this.min.set(0,0,0);
+    this.max.set(0,0,0);
+  }
+}
+
+export class BoundingSphere {
+  constructor() {
+    this.debug = true;
+    this.center = new THREE.Vector3();
+    this.radius = 0;
+    //this.sphere?
+  }
+
+  reset() {
+    this.center.set(0,0,0);
+    this.radius = 0;
+  }
+}
+
+export class Area {
+  constructor() {
+
+  }
+
+  reset() {
+
+  }
+}
+
+export class AreaEntering extends TagComponent {}
+export class AreaExiting extends TagComponent {}
+export class AreaInside extends TagComponent {}
+
+export class AreaChecker {
+  constructor() {
+
+  }
+
+  reset() {
+
+  }
+}
+
+export class DebugHelper extends TagComponent {}
