239512ff2428d5b893b2aa6787fef12f9f57cf11
Fernando Serrano <fernandojsg@gmail.com>
2019-11-14 12:14:59 +0100

    Move functions to utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,3 +9,12 @@ export function newMarker(x, y, z, color){
   }
   return mesh;
 }
+
+export function angleBetween(point1, point2) {
+  return Math.atan2(point2.x - point1.x, point2.y - point1.y);
+}
+
+export function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
