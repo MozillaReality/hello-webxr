6108ac2ea2610cc04e07459c4447994b11e164ad
Brian Peiris <brianpeiris@gmail.com>
2020-01-31 19:45:20 -0800

    Remove preset-env to fix babel class error

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,7 @@ module.exports = {
     {
       test: /\.(js|mjs)$/,
       exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env']
-        }
-      }
+      use: { loader: 'babel-loader' }
     }
   ]
   },
